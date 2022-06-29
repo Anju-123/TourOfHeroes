a352df1791066784e8814d975b313958746ab584
Anju-123 <anjujosemaliyackal@gmail.com>
2022-06-29 16:49:55 +0200

    3 files modified

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -16,12 +16,12 @@ export class HeroesComponent implements OnInit {
 
     heroes=HEROES;
     selectedHero?: Hero;
-    onSelect(hero: Hero): void {
-      this.selectedHero = hero;
-    }
+
   constructor() { }
 
   ngOnInit() : void{
   }
-
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
